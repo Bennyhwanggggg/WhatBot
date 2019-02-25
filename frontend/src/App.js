6c4e4b45f70f6975811f87b3fabc148635d84ad8
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-02-25 11:47:32 +1100

    remove useless contents

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="WhatBot">
+        <header className="WhatBot">
         </header>
       </div>
     );
