76fc09ebf41247e9ba8dfd0577607cdae0ec8d4c
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-02-25 15:37:06 +1100

    update classname and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app">
+      <div className="App">
         <div className="App-header">
           <h1>WhatBot</h1>
         </div>
