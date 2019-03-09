b7dad4927684e6b4bc0ff75b998aab70ed62e163
Wayne <mingyuewayne@gmail.com>
2019-03-09 11:28:51 +1100

    try API for login

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,28 @@
-import flask
+from flask import (Flask, request, abort, jsonify)
 
-app = flask.Flask("__main__")
+app = Flask(__name__)
 
+users = []
 
 @app.route('/')
 def my_index():
-    return flask.render_template("index.html", token="Hello Flask+React")
+    #return flask.render_template("index.html", token="Hello Flask+React")
+    return "hello"
+
+
+@app.route('/login', methods = ["post"])
+def login():
+    #if you do not pass username, then the default will be None
+    username = request.json.get('username', None)
+    if username is None or username in users:
+        abort(401)
+    else:
+        users.append(username)
+        print(users)
+        return jsonify({'status': 'OK', 'message': 'Successfully logged in'})
 
 
 
-app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
