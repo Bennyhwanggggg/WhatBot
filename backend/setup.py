1af248885abf3c942686aecca1b85341b1c55605
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-03-23 16:41:58 +1100

    added setup and update gitignore for build

---
+++ backend/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='WhatBot Backend',
+    version='0.0.1',
+    description='WhatBot backend modules',
+    packages=['conf',
+              'data_extractor',
+              'database',
+              'query_module',
+              'response_module']
+)
