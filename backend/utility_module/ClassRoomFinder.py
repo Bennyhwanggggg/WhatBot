c159989328e1878f7e00a1452f80f8df0cd16f92
Wayne <mingyuewayne@gmail.com>
2019-03-28 22:26:16 +1100

    adding data function for looking for class room

--- backend/utility_module/ClassRoomFinder.py
+++ backend/utility_module/ClassRoomFinder.py
@@ -1,3 +1,5 @@
+from database.DataBaseManager import DataBaseManager
+
 """
     The ClassRoomFinder class is responsible for providing user
     the class room and tutorial locator feature.
@@ -5,8 +7,15 @@
 
 
 class ClassRoomFinder:
-    def __init__(self, class_room_table):
-        self.class_room_table = class_room_table
+    def __init__(self, data_base_manager=DataBaseManager()):
+        self.data_base_manager = data_base_manager
 
-    def find_class_room(self, class_room):
-        pass
+    def find_class_room(self, cid):
+        query = "SELECT * from classroom where cid like %s"
+        inputs = (cid, )
+        return self.data_base_manager.execute_query(query, inputs)
+
+
+if __name__ == '__main__':
+    data_base_manager = ClassRoomFinder()
+    result = data_base_manager.find_class_room("COMP990")
