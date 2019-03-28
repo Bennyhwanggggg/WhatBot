9ef037501d2043ecf3c13f61dab549c987b70956
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-03-28 12:30:48 +1100

    remove unused test

--- backend/tests/test_consultation.py
+++
@@ -1,6 +0,0 @@
-from consultation.DataBaseManager import Consultation
-if __name__ == "__main__":
-    consultation = Consultation()
-    array = consultation.add_consultation("COMP9900", "z123456", "9:00AM", '{2013-06-01}')
-    array = consultation.check_avail_timeslot("COMP9900")
-    array = consultation.delete_consultation("COMP9900", "z123456", "9:00AM")
