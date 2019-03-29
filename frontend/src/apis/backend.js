e2d5c7aa3e21b7dd6aed67ffa30a52fdbf7d7a3f
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-03-29 19:59:58 +1100

    configure frontend environment and server connection

--- frontend/src/apis/backend.js
+++ frontend/src/apis/backend.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const backendURL = process.env.NODE_ENV === 'development' ? 'http://localhost:9999' : `http://${process.env.REACT_APP_BACKEND_NAME}:${process.env.REACT_APP_BACKEND_PORT}`
+
 export default axios.create({
-    baseURL: 'http://localhost:9999'
-})
+    baseURL: backendURL
+});
