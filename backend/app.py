af8eac95039950dfe301e795d4af405b8691e047
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-03-30 23:18:53 +1100

    add upload file endpoint and enums for response messages

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,50 @@
-from flask import (Flask, request, abort, jsonify)
+from flask import Flask, request, abort, jsonify, flash
 from flask_cors import CORS
+from werkzeug.utils import secure_filename
 from datetime import datetime
 import uuid
+import os
 from query_module.QueryModule import QueryModule
 from response_module.ResponseModule import ResponseModule
-from conf.Response import IntentResponse, FallbackResponse
+from conf.Error import UploadFileError
+from conf.Success import UploadFileSuccess
 
+"""
+    Initialize modules
+"""
 query_module = QueryModule()
 response_module = ResponseModule()
 
+"""
+    Flask configuration setup
+"""
 app = Flask(__name__)
 CORS(app)
+UPLOAD_FOLDER = '/Users/mitsunari/Documents/UNSW/COMP9900-Info-Tech-Project/project/backend'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max size
+ALLOWED_EXTENSIONS = set(['txt'])
 
 
 @app.route('/login', methods=["post"])
 def login():
     pass
+
+
+@app.route('/upload', methods=["post"])
+def upload():
+    def allowed_file(f):
+        return '.' in f and f.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+    if 'file' not in request.files:
+        return jsonify(message=UploadFileError.NO_FILE.value), 400
+    file = request.files['file']
+    if not file.filename:
+        return jsonify(message=UploadFileError.NO_FILE_SELECTED.value)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename), 400
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify(message=UploadFileSuccess.SUCCESS.value), 200
 
 
 @app.route('/message', methods=['POST'])
---
+++ backend/conf/Success.py
@@ -0,0 +1,5 @@
+import enum
+
+
+class UploadFileSuccess(enum.Enum):
+    SUCCESS = 'File(s) successfully uploaded'
