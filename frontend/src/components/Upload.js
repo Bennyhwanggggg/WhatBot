dc6d165c9753b9c77792f102e3aabf6c76c4a591
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-03-30 23:25:47 +1100

    init upload and its css

---
+++ frontend/src/components/Upload.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Upload extends Component {
+    constructor(props) {
+      super(props)
+    }
+  
+    render() {
+        return (
+          <div className="Upload">
+            <span className="Title">Upload Files</span>
+            <div className="Content">
+              <div />
+              <div className="Files" />
+            </div>
+            <div className="Actions" />
+          </div>
+        )
+      }
+  }
