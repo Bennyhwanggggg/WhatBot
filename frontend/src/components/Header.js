6dd4aa2435392534337708c4ec9e16fb92815241
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-03-31 09:20:54 +1100

    make training data upload its own page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import Upload from './Upload';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
       <div className="ui pointing menu App-header">
         <div className="header item">
-            WhatBot
+            <Link to="/">
+                WhatBot
+            </Link>
         </div>
         <div className="item">
-          <Upload />
+          <Link to="/upload">
+            Upload training data
+          </Link>
         </div>
         <div className="right item">
             Login
         </div>
       </div>
     );
-  };
+};
   
-  export default Header;
+export default Header;
