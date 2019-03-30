9da637ffdec45ade3e274bde5640e2be5ab55a7e
Wayne <mingyuewayne@gmail.com>
2019-03-30 17:14:42 +1100

    improve pandas for classroom

--- backend/utility_module/ClassRoomFinder.py
+++ backend/utility_module/ClassRoomFinder.py
@@ -7,21 +7,23 @@ import pandas as pd
 """
 
 class ClassRoomFinder:
-    def __init__(self, data_base_manager=DataBaseManager()):
+    def __init__(self, data_base_manager=DataBaseManager(), data='data'):
         self.data_base_manager = data_base_manager
+        self.data = data
 
     def find_all_classroom(self):
         query = "SELECT * from classroom"
         result = self.data_base_manager.execute_query(query)
         df = pd.DataFrame(data = result, columns=['course', 'location'])
-        df.to_csv('classRoom.csv')
+        self.data = df
+        return df
 
     def find_class_room(self, cid):
-        location = pd.read_csv('classRoom.csv')
-        isCourse = location['course'] == cid
-        result = location[isCourse].location.head().values
-        if result:
-            final = location[isCourse].location.head().values[0]
+        listLocation = self.data
+        isCourse = listLocation['course'] == cid
+        result = listLocation[isCourse].head().values
+        if result.any():
+            final = listLocation[isCourse].head().values[0][1]
             return final
         else:
             return "No such course"
@@ -29,6 +31,5 @@ class ClassRoomFinder:
 
 if __name__ == '__main__':
     data_base_manager = ClassRoomFinder()
-    result = data_base_manager.find_class_room("COMP9900")
     #result = data_base_manager.find_all_classroom()
-    #print(result)
+    #result = data_base_manager.find_class_room("COMP93")
