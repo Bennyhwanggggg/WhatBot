543d3ccf3056b27b16d27fef0ebdb2cc86f18e7a
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-03-30 23:38:27 +1100

    remove useless constructor

--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 class Upload extends Component {
-    constructor(props) {
-      super(props)
-    }
-  
     render() {
         return (
           <div className="Upload">
