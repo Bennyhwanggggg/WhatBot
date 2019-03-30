47fa8e15194f209060001e8a43737513b6b86b62
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-03-30 18:00:06 +1100

    update requirements and refactor class room finder

--- backend/utility_module/ClassRoomFinder.py
+++ backend/utility_module/ClassRoomFinder.py
@@ -7,29 +7,24 @@ import pandas as pd
 """
 
 class ClassRoomFinder:
-    def __init__(self, data_base_manager=DataBaseManager(), data='data'):
+    def __init__(self, data_base_manager=DataBaseManager()):
         self.data_base_manager = data_base_manager
-        self.data = data
+        self.data = None
 
-    def find_all_classroom(self):
+    def get_all_classroom(self):
         query = "SELECT * from classroom"
         result = self.data_base_manager.execute_query(query)
-        df = pd.DataFrame(data = result, columns=['course', 'location'])
-        self.data = df
-        return df
+        self.data = pd.DataFrame(data=result, columns=['course', 'location'])
+        self.data.set_index('course', inplace=True)
 
     def find_class_room(self, cid):
-        listLocation = self.data
-        isCourse = listLocation['course'] == cid
-        result = listLocation[isCourse].head().values
-        if result.any():
-            final = listLocation[isCourse].head().values[0][1]
-            return final
-        else:
-            return "No such course"
+        if self.data is None:
+            self.get_all_classroom()
+        return self.data.loc[cid].values[0] if cid in self.data.index else 'No information for the course queried'
 
 
 if __name__ == '__main__':
-    data_base_manager = ClassRoomFinder()
-    #result = data_base_manager.find_all_classroom()
-    #result = data_base_manager.find_class_room("COMP93")
+    class_room_finder = ClassRoomFinder()
+    class_room_finder.get_all_classroom()
+    result = class_room_finder.find_class_room("COMP9900")
+    print(result)
