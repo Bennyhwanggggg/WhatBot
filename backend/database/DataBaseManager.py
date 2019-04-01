4093b6bef71432d6d86c37e6da8ab9744620a15f
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-04-01 13:12:02 +1100

    add processing data and read from S3

--- backend/database/DataBaseManager.py
+++ backend/database/DataBaseManager.py
@@ -81,24 +81,80 @@ class DataBaseManager:
         return result
 
     def connect_file_storage(self):
+        """Used to establish AWS S3 bucket connections and instances with the provided access keys
+
+        :return: None
+        """
         if self.s3_resource is None:
             self.s3_resource = boto3.resource('s3',
                                               aws_access_key_id=aws_access_key_id,
                                               aws_secret_access_key= aws_secret_access_key)
+            logger.info('Connection to file storage established')
+        else:
+            logger.warning('Connection to file storage already exist')
 
     def disconnect_file_storage(self):
+        """Used to disconnect from S3 buckets.
+
+        :return: None
+        """
         if self.s3_resource is not None:
             self.s3_resource = None
+            logger.info('Connection to file storage closed')
+        else:
+            logger.warning('No connection to file storage to disconnect from')
 
     def upload_file(self, path_to_file, file_name):
+        """Used to upload file to S3 bucket for file storage.
+
+        :param path_to_file: path of the file to upload to
+        :type: str
+        :param file_name: the file name you want it to be stored as on S3
+        :type: str
+        :return: None
+        """
         self.connect_file_storage()
         self.s3_resource.Bucket(self.file_storage_name).upload_file(Filename=path_to_file, Key=file_name)
         self.disconnect_file_storage()
 
     def download_file(self, file_name, path_to_download_to):
+        """Download file from S3
+
+        :param file_name: file on S3 you want to download
+        :type: str
+        :param path_to_download_to: location where you want to download the file to
+        :type: str
+        :return: None
+        """
         self.connect_file_storage()
         self.s3_resource.Bucket(self.file_storage_name).download_file(Key=file_name, Filename=path_to_download_to)
         self.disconnect_file_storage()
+
+    def read_file(self, file_name):
+        """Read a file from S3 without downloading. Converts the bytes data into string.
+
+        :param file_name: file you want to read
+        :type: str
+        :return: file content
+        :rtype: str
+        """
+        self.connect_file_storage()
+        binary_data = self.s3_resource.Object(self.file_storage_name, file_name).get()['Body'].read()
+        self.disconnect_file_storage()
+        return self.process_string_data(binary_data.decode('utf-8'))
+
+    def process_string_data(self, string_data, separator='\n'):
+        """Convert string data into lists using a separator. By default it is new line ('\n').
+        This is used mainly to process the data we read from S3
+
+        :param string_data: string data to process
+        :type: str
+        :param separator: separator to use for processing
+        :type: str
+        :return: list of strings separated by the specified separator
+        :rtype: list[str]
+        """
+        return [x for x in string_data.split(separator)]
 
     """
         Common SQL query operations
@@ -186,9 +242,3 @@ class DataBaseManager:
         query = "SELECT pdf_url from info_handbook where cid like %s"
         inputs = (key_part,)
         return self.execute_query(query, inputs)
-
-
-
-if __name__ == '__main__':
-    data_base_manager = DataBaseManager()
-    data_base_manager.upload_file('./schemas/9900_database.sql', 'test')
