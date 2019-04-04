b0ac982a4cf04894c5c0912dc540473c0c0752f0
Wayne <mingyuewayne@gmail.com>
2019-04-04 14:42:05 +1100

    improve consultation

--- backend/tests/test_consultation_manager.py
+++ backend/tests/test_consultation_manager.py
@@ -18,3 +18,18 @@ def test_valid_booking_time():
         test = consultation_manager.round_time(test)
         result = consultation_manager.check_valid_booking_time(test)
         assert expect == result
+
+
+def test_consultation_booking_query():
+    consultation_manager = ConsultationManager()
+    inputs = [["COMP9900", "z5111111", "09:00", "2019-04-10"], ["COMP9900", "z5111111", "09:00", "2019-04-10"],["COMP9900", "z5111111", "10:00", "2019-04-07"]]
+    expected_1 = "execute successfully Your booking is on next Wednesday"
+    expected_2 = "Sorry this time slot has been booked, please choose another one from following time slots on 2019-04-1010:00:00, 11:00:00, 12:00:00, 13:00:00, 14:00:00, 15:00:00, 16:00:00, 17:00:00"#availabale tiemslot list
+    expected_3 = "Sorry, there is no consultation on weekends"
+    compare = [expected_1, expected_2, expected_3]
+    for i in range(len(inputs)):
+
+        result = consultation_manager.consultation_booking_query(inputs[i][0], inputs[i][1], inputs[i][2], inputs[i][3])
+        print(result)
+        assert result == compare[i]
+
--- backend/utility_module/ConsultationManager.py
+++ backend/utility_module/ConsultationManager.py
@@ -3,7 +3,10 @@
     operations relating to the consultation booking feature.
 """
 from database.DataBaseManager import DataBaseManager
+import datetime
+from conf.Logger import Logger
 
+logger = Logger(__name__).log
 
 class ConsultationManager:
     def __init__(self, data_base_manager=DataBaseManager()):
@@ -17,9 +20,9 @@ class ConsultationManager:
         inputs = (cid, sid, time, date)
         return self.data_base_manager.execute_query(query, inputs)
 
-    def delete_consultation(self, cid, sid, time):
-        query = "DELETE FROM consultation WHERE cid = %s and sid = %s and time = %s"
-        inputs = (cid, sid, time)
+    def delete_consultation(self, cid, sid, time, date):
+        query = "DELETE FROM consultation WHERE cid = %s and sid = %s and time = %s and date = %s"
+        inputs = (cid, sid, time, date)
         return self.data_base_manager.execute_query(query, inputs)
 
     def update_consultation(self):#update the consultation database everyday
@@ -28,6 +31,18 @@ class ConsultationManager:
             query = "DELETE FROM consultation WHERE date < %s"
             inputs = (key_part, )
             return self.data_base_manager.execute_query(query, inputs)
+
+    def next_seven_day(self):# getting the date of 7 days later, used in check_weekday first if condition
+        today = datetime.date.today()
+        week_next = today + datetime.timedelta(days=7)
+        return week_next.strftime('%Y-%m-%d')
+
+    def update_consultation(self):#update the consultation database everyday
+        date = datetime.datetime.today().strftime('%Y-%m-%d')#eg. 2019-03-28
+        key_part = '%' + date
+        query = "DELETE FROM consultation WHERE date < %s"
+        inputs = (key_part, )
+        return self.data_base_manager.execute_query(query, inputs)
 
     def check_weekday(self,date):#check whether the booking day is valid, date like 2019-03-28
         week_next = self.next_seven_day()#get the date of 7 days later from current date
@@ -42,36 +57,62 @@ class ConsultationManager:
             day = datetime.date(date_list[0], date_list[1], date_list[2])# eg. should be 2017,12,25 integer parameters; <class 'datetime.date'>
             num_day = day.weekday()# convert weekday into digit (eg Mon -> 0,)
             if num_day == 5 or num_day == 6:# check whether it is on weekend
-                print("Sorry, there is no consultation on weekends")
+                logger.info("Sorry, there is no consultation on weekends")
                 return False, "Sorry, there is no consultation on weekends"
             else:
                 DayAsString = weekDays[num_day]
-                print("It is on next", DayAsString)
-                return True, "It is on next " + DayAsString
+                logger.info("It is on next" + DayAsString)
+                return True, "Your booking is on next " + DayAsString
         except ValueError:
-            print("Wrong input date")
+            logger.warn("Warning: Wrong input date")
             return False, "Please try again"
+
+    def check_avail_timeslot(self, cid, date):
+        initial_timeslots = ['09:00:00',
+                              '10:00:00',
+                              '11:00:00',
+                              '12:00:00',
+                              '13:00:00',
+                              '14:00:00',
+                              '15:00:00',
+                              '16:00:00',
+                              '17:00:00']
+
+        query = "SELECT time from consultation where cid = %s and date = %s"
+        inputs = (cid, date)
+        array_book = self.data_base_manager.execute_query(query, inputs)
+        array_book = [e[0] for e in array_book]
+        booked = array_book if array_book else []
+        avail_timeslot = []
+        for time in initial_timeslots:
+            if time not in booked:
+                logger.info(time)
+                avail_timeslot.append(time)
+        return avail_timeslot
 
     def consultation_booking_query(self, cid, sid, time, date):
         is_weekday, feedback = self.check_weekday(date)
         if is_weekday:
             try:
                 avail_list = self.check_avail_timeslot(cid, date)#return available timeslot list
+                time = time + ":00"
+                print("time", time)
+                print("avail_list", avail_list)
                 if time in avail_list:
                     result = self.add_consultation(cid, sid, time, date)# add into database
-                    print(result +"\n" +feedback)
-                    return result +"\n"+ feedback
+                    logger.info(result+"\n"+feedback)
+                    return result +" " + feedback
                 else:
                     if not avail_list:
                         return "Sorry, there is no available time slot on date"
                     result = "Sorry this time slot has been booked, please choose another one from following time slots on " + date
-                    print(avail_list)
+                    logger.info(avail_list)
                     return result + ', '.join(avail_list)
             except ValueError:
-                print("Invalid Input")
+                logger.debug("Invalid Input")
                 return
         else:
-            print(feedback)
+            logger.warn(feedback)
             return feedback
 
 
@@ -115,4 +156,5 @@ class ConsultationManager:
         :rtype: str
         """
         hour, mins, _ = time.split(":")
-        return '{:02d}:00:00'.format(int(hour)+1) if int(mins) >= 30 else '{:02d}:00:00'.format(int(hour))
+        return '{:02d}:00:00'.format(int(hour)+1 ) if int(mins) >= 30 else '{:02d}:00:00'.format(int(hour))
+
