18aa1ad6a0a600b827756de31b130e12b3f44114
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-04-10 16:12:29 +1000

    fix message bubble to only show one from now

--- frontend/src/reducers/messagesReducer.js
+++ frontend/src/reducers/messagesReducer.js
@@ -11,10 +11,32 @@ export default (state = INITIAL_STATE, action) => {
             return { ...state, messages: [...state.messages, action.payload] }
         case MESSAGE_RECIEVED:
             action.payload.id = action.id
+            var messageList = state.messages.map(message => (message.id === action.id) ? action.payload : message)
+            var messages = []
+            var loadingMessages = []
+            for (var i=0; i<messageList.length; i++) {
+                if (messageList.message && messageList.message !== 'loading') {
+                    loadingMessages.push(messageList[i])
+                } else {
+                    messages.push(messageList[i])
+                }
+            }
+            state.messages = messages.concat(loadingMessages)
             return { ...state, messages: state.messages.map(message => (message.id === action.id) ? action.payload : message) }
         case MESSAGE_LOADING:
             action.payload.id = action.id
-            return { ...state, messages: [...state.messages, action.payload] }
+            var messageList = [...state.messages, action.payload]
+            var messages = []
+            var loadingMessages = []
+            for (var i=0; i<messageList.length; i++) {
+                if (messageList[i].message && messageList[i].message === 'loading') {
+                    loadingMessages.push(messageList[i])
+                } else {
+                    messages.push(messageList[i])
+                }
+            }
+            messages = messages.concat(loadingMessages)
+            return { ...state, messages: messages}
         default:
             return state
     }
