c892353143c068367d4bdbc79f0df6d7a2a68d17
Wayne <mingyuewayne@gmail.com>
2019-04-11 22:12:25 +1000

    modify wam test file

--- backend/tests/test_wam_calculator.py
+++ backend/tests/test_wam_calculator.py
@@ -2,33 +2,33 @@ from utility_module.WamCalculator import WamCalculator
 
 def test_wam_calculation():
     wam = WamCalculator()
-    result = wam.calculate_wam("z8888888")
+    result = wam.calculate_wam("z1234567")
 
     expected = 'Course name: COMP6441\n' \
                'Number of credits: 6\n' \
-               'Result: 85.0\n' \
+               'Result: 95.0\n' \
                'Course name: COMP9020\n' \
                'Number of credits: 6\n' \
-               'Result: 95.0\n' \
+               'Result: 98.0\n' \
                'Course name: COMP9021\n' \
                'Number of credits: 6\n' \
-               'Result: 99.0\n' \
+               'Result: 100.0\n' \
                'Course name: COMP9032\n' \
                'Number of credits: 6\n' \
-               'Result: 67.0\n' \
+               'Result: 78.0\n' \
                'Course name: COMP9311\n' \
                'Number of credits: 6\n' \
-               'Result: 77.0\n' \
+               'Result: 88.0\n' \
                'Course name: COMP9414\n' \
                'Number of credits: 6\n' \
                'Result: 66.0\n' \
                'Course name: COMP9814\n' \
                'Number of credits: 6\n' \
-               'Result: 86.0\n' \
+               'Result: 79.0\n' \
                'Course name: COMP9511\n' \
                'Number of credits: 6\n' \
-               'Result: 74.0\n' \
-               'Wam is: 81.1\n' \
+               'Result: 85.0\n' \
+               'Wam is: 86.1\n' \
                'Grade is: D'
 
 
