64904da1264b990af47c011fb48f5fd426b2e550
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-04-11 17:57:44 +1000

    init dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import ReactEcharts from 'echarts-for-react'; 
 
 class Dashboard extends React.Component {
+
+    sample_chart_option = () => {
+        
+    }
+
+    generateChart = () => {
+        return (
+            <ReactEcharts
+                option={this.getOption()}
+                notMerge={true}
+                lazyUpdate={true}
+                theme={"theme_name"}
+                onChartReady={this.onChartReadyCallback}
+                onEvents={EventsDict}
+                opts={} />
+        )
+    }
+
     render() {
         return (
-            <div>This is a dashboard page</div>
+            <div>{this.generateChart()}</div>
         )
     }
 }
