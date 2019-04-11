74959f17f50d2f57f66a376825e59ef44cc86b87
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-04-11 22:03:16 +1000

    finish rough version

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -4,19 +4,58 @@ import ReactEcharts from 'echarts-for-react';
 class Dashboard extends React.Component {
 
     sample_chart_option = () => {
-        
+        var option = {
+            tooltip: {
+                trigger: 'item',
+                formatter: "{a} <br/>{b}: {c} ({d}%)"
+            },
+            legend: {
+                orient: "vertical",
+                x: "left",
+                data:["cconsultation_booking","prerequisites_queries","indicative_hours_queries","course_outline_queries","course_location_queries"]
+            },
+            series: [
+                {
+                    name:"intents",
+                    type:"pie",
+                    radius: ["50%", "70%"],
+                    avoidLabelOverlap: false,
+                    label: {
+                        normal: {
+                            show: false,
+                            position: "center"
+                        },
+                        emphasis: {
+                            show: true,
+                            textStyle: {
+                                fontSize: "30",
+                                fontWeight: "bold"
+                            }
+                        }
+                    },
+                    labelLine: {
+                        normal: {
+                            show: false
+                        }
+                    },
+                    data:[
+                        {value:335, name:"consultation_booking"},
+                        {value:310, name:"prerequisites_queries"},
+                        {value:234, name:"indicative_hours_queries"},
+                        {value:1135, name:"course_outline_queries"},
+                        {value:1548, name:"course_location_queries"}
+                    ]
+                }
+            ]
+         };
+         return option;
     }
 
     generateChart = () => {
         return (
             <ReactEcharts
-                option={this.getOption()}
-                notMerge={true}
-                lazyUpdate={true}
-                theme={"theme_name"}
-                onChartReady={this.onChartReadyCallback}
-                onEvents={EventsDict}
-                opts={} />
+                option={this.sample_chart_option()}
+                 />
         )
     }
 
