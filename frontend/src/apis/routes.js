da2d2cd95792cd757345e5d46414eb0d9cfe8876
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-04-12 15:15:13 +1000

    fix import error

--- frontend/src/apis/routes.js
+++ frontend/src/apis/routes.js
@@ -1,3 +1,5 @@
 const dev = 'http://localhost:9999';
-const prod = `https://${process.env.REACT_APP_BACKEND_NAME}.herokuapp.com`
-export const backendURL = process.env.NODE_ENV === 'development' ? dev : prod
+const prod = `https://${process.env.REACT_APP_BACKEND_NAME}.herokuapp.com`;
+const backendURL = process.env.NODE_ENV === 'development' ? dev : prod;
+
+export default backendURL;
