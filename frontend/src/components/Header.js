76c622f931a713b65c06d97535c504a57ffdad26
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-04-13 20:41:14 +1000

    fix connect in header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { signOut } from '../actions';
 
 class Header extends React.Component {
