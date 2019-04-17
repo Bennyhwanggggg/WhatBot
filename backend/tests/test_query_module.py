e0373fa13823bfe261a69c8238d85dc057fea8ca
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-04-17 18:24:07 +1000

    update test case for consultatio ncancel

--- backend/tests/test_query_module.py
+++ backend/tests/test_query_module.py
@@ -302,7 +302,7 @@ def test_consultation_cancelling_command():
                      'Cancel course consultation for COMP9334 at 06:45 20/05/19',
                      'Cancel booking for COMP9334 on 06:45 20/05/19',
                      'Cancel course consultation for COMP9334 at 06:45 on 20/05/19',
-                     'Make a consultation cancel for COMP9334 on 20/05/19 at 06:45',
+                     'Make a consultation cancellation for COMP9334 on 20/05/19 at 06:45',
                      'I want to cancel a course consultation on 20/05/19 at 06:45 for COMP9334?']
     for test_message in test_messages:
         result = query_module.detect_intent_texts(test_message)
--- backend/tests/test_query_module_followups.py
+++ backend/tests/test_query_module_followups.py
@@ -291,6 +291,7 @@ def test_consultation_cancelling_user_input_course_code_first_followup_1():
 
     time.sleep(TIME_BETWEEN_API)
 
+
 def test_consultation_cancelling_user_input_course_code_first_followup_2():
     query_module = QueryModule()
 
@@ -351,7 +352,7 @@ def test_consultation_cancelling_user_input_time_date_first_followup_2():
 
     time.sleep(TIME_BETWEEN_CONTEXT)
 
-    test_message = "Make a cancel at 11am on 04/09/19"
+    test_message = "Cancel the booking at 11am on 04/09/19"
     result = query_module.detect_intent_texts(test_message)
     assert result.intent == 'consultation_cancelling_with_followup-user_input_time_and_date_with_followup'
     assert result.message == 'Sure! Please tell me the course code of the course you want to cancel consultation for?'
