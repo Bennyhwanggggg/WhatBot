16d5cabb4a6bf4afee2dbc0b3db64b8a427233ec
Wayne <mingyuewayne@gmail.com>
2019-04-17 20:22:05 +1000

    adding confirmation email with booking and cancel

--- backend/emailer/EmailSender.py
+++ backend/emailer/EmailSender.py
@@ -1,5 +1,5 @@
 import smtplib
-from email.config import EMAIl_ADDRESS, PASSWORD
+from config import EMAIl_ADDRESS, PASSWORD
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from conf.Logger import Logger
@@ -43,3 +43,68 @@ class EmailSender:
             logger.info("Success: Email sent to {}!".format(receiver))
         except Exception as e:
             logger.error(str(e))
+
+
+
+    def send_confirm_booking(self, cid, receiver, date, time):
+        try:
+            server = smtplib.SMTP('smtp.gmail.com:587')
+            server.ehlo()
+            server.starttls()
+            server.login(self.email_address, self.password)
+            message = MIMEMultipart("alternative")
+            message["Subject"] = "Booking confirmation"
+
+            html = """\
+            <html>
+              <body>
+                <p>Hi,<br>
+                   You have successfully booked at {} {} with {}
+                </p>
+              </body>
+            </html>
+            """.format(time, date, cid)
+
+            part1 = MIMEText(html, "html")
+            message.attach(part1)
+
+            server.sendmail(self.email_address, receiver, message.as_string())
+            server.quit()
+            logger.info("Success: Email sent to {}!".format(receiver))
+        except Exception as e:
+            logger.error(str(e))
+
+
+
+    def send_confirm_cancelling(self, cid, receiver, date, time):
+        try:
+            server = smtplib.SMTP('smtp.gmail.com:587')
+            server.ehlo()
+            server.starttls()
+            server.login(self.email_address, self.password)
+            message = MIMEMultipart("alternative")
+            message["Subject"] = "Cancelling confirmation"
+
+            html = """\
+            <html>
+              <body>
+                <p>Hi,<br>
+                   You have successfully cancelled at {} {} with {}
+                </p>
+              </body>
+            </html>
+            """.format(time, date, cid)
+
+            part1 = MIMEText(html, "html")
+            message.attach(part1)
+
+            server.sendmail(self.email_address, receiver, message.as_string())
+            server.quit()
+            logger.info("Success: Email sent to {}!".format(receiver))
+        except Exception as e:
+            logger.error(str(e))
+
+
+#if __name__ == '__main__':
+    #EmailSender = EmailSender()
+    #EmailSender.send_confirm_cancelling("COMP9900", "whatbot9900@gmail.com", "20-05-2019", "06:45")
