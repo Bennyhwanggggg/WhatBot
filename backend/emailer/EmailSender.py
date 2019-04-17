3299793b07b023b82c0e29a89fe3902bd723fc9b
Mingyue Wayne <mingyuewayne@gmail.com>
2019-04-17 20:36:33 +1000

    Dev/sending email (#145)
    * adding confirmation email with booking and cancel
    
    * improve email message

--- backend/emailer/EmailSender.py
+++ backend/emailer/EmailSender.py
@@ -1,5 +1,5 @@
 import smtplib
-from email.config import EMAIl_ADDRESS, PASSWORD
+from config import EMAIl_ADDRESS, PASSWORD
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from conf.Logger import Logger
@@ -34,6 +34,62 @@ class EmailSender:
               </body>
             </html>
             """.format(cid, msg)
+
+            part1 = MIMEText(html, "html")
+            message.attach(part1)
+
+            server.sendmail(self.email_address, receiver, message.as_string())
+            server.quit()
+            logger.info("Success: Email sent to {}!".format(receiver))
+        except Exception as e:
+            logger.error(str(e))
+
+    def send_confirm_booking(self, cid, receiver, date, time):
+        try:
+            server = smtplib.SMTP('smtp.gmail.com:587')
+            server.ehlo()
+            server.starttls()
+            server.login(self.email_address, self.password)
+            message = MIMEMultipart("alternative")
+            message["Subject"] = "Booking confirmation"
+
+            html = """\
+            <html>
+              <body>
+                <p>Hi,<br>
+                   You have successfully booked a consultation for {} at {} on {}
+                </p>
+              </body>
+            </html>
+            """.format(time, date, cid)
+
+            part1 = MIMEText(html, "html")
+            message.attach(part1)
+
+            server.sendmail(self.email_address, receiver, message.as_string())
+            server.quit()
+            logger.info("Success: Email sent to {}!".format(receiver))
+        except Exception as e:
+            logger.error(str(e))
+
+    def send_confirm_cancelling(self, cid, receiver, date, time):
+        try:
+            server = smtplib.SMTP('smtp.gmail.com:587')
+            server.ehlo()
+            server.starttls()
+            server.login(self.email_address, self.password)
+            message = MIMEMultipart("alternative")
+            message["Subject"] = "Cancelling confirmation"
+
+            html = """\
+            <html>
+              <body>
+                <p>Hi,<br>
+                   You have successfully cancelled your consultation booking for {} at {} on {}
+                </p>
+              </body>
+            </html>
+            """.format(time, date, cid)
 
             part1 = MIMEText(html, "html")
             message.attach(part1)
