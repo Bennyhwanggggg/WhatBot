c62e1d607743a25adf8dbe5fc3398df8bc4bc283
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-04-18 21:24:19 +1000

    testing..

--- frontend/src/apis/routes.js
+++ frontend/src/apis/routes.js
@@ -1,5 +1,5 @@
 const dev = 'http://localhost:9999';
-const prod = `https://${process.env.REACT_APP_BACKEND_NAME}.herokuapp.com`;
+const prod = `http://localhost:9999`;
 const backendURL = process.env.NODE_ENV === 'development' ? dev : prod;
 
 export default backendURL;
