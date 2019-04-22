11babd725f34cdd819a05f461d90f5bef560a6ba
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-04-22 14:23:30 +1000

    update start script to not use second cd

--- start_server.sh
+++ start_server.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-# setup dependencies for backend
-(cd backend || exit; python3 -m pip install virtualenv --user; python3 -m virtualenv venv; source venv/bin/activate; python3 -m pip install -r requirements.txt; python3 setup.py install) &
-# setup dependencies for frontend
-(cd frontend || exit; npm install)
-
-cd ..
-
-# run backend then frontend
-(cd backend || exit; python3 app.py) &
-cd frontend || exit; npm start
-
+# setup dependencies for backend then launch
+(cd backend || exit; python3 -m pip install virtualenv --user; python3 -m virtualenv venv; source venv/bin/activate; python3 -m pip install -r requirements.txt; python3 setup.py install; python3 app.py) &
+# setup dependencies for frontend then run
+(cd frontend || exit; npm install; npm start)
