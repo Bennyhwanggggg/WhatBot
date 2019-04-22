2415127e05ad2573e449235a13a41be153de8889
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-04-22 14:46:48 +1000

    update to fix stuck bug

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+# setup dependencies for frontend
+(cd frontend || exit; npm install) & 
 # setup dependencies for backend then launch
-(cd backend || exit; python3 -m pip install virtualenv --user; python3 -m virtualenv venv; source venv/bin/activate; python3 -m pip install -r requirements.txt; python3 setup.py install; python3 app.py) &
-# setup dependencies for frontend then run
-(cd frontend || exit; npm install; npm start)
+(cd backend || exit; python3 -m pip install virtualenv --user; python3 -m virtualenv venv; source venv/bin/activate; python3 -m pip install -r requirements.txt; python3 setup.py install; python3 app.py) 
+
+cd frontend
+npm start
