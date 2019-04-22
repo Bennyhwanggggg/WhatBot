813598f75a0fd45c1281e443c0619760b4df518c
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-04-22 14:07:29 +1000

    update start server script to do setup

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# setup dependencies for backend
+(cd backend || exit; python3 -m pip install virtualenv --user; python3 -m virtualenv venv; source venv/bin/activate; python3 -m pip install -r requirements.txt; python3 setup.py install) &
+# setup dependencies for frontend
+(cd frontend || exit; npm install)
 
+cd ..
+
+# run backend then frontend
 (cd backend || exit; python3 app.py) &
-
 cd frontend || exit; npm start
 
